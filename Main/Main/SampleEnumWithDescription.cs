ca5a5a7a53af4caff46ca6e3305b6397a1d07e6b
Qivikavi <61878419+Qivikavi@users.noreply.github.com>
2021-01-29 18:11:13 +1000

    Добавьте файлы проекта.

---
+++ Main/Main/SampleEnumWithDescription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace Main
+{
+    public enum SampleEnumWithDescription
+    {
+        [Display(Description = "First")]
+        One,
+        [Display(Description = "Second")]
+        Two,
+        [Display(Description = "Third")]
+        Three
+    }
+}
